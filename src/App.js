e97c97ee653a70a3646b7e1db7e00264ea6318b1
Artemia <mute08@naver.com>
2022-04-10 15:57:53 +0900

    init n style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./App.css"
 
 function App() {
-  return <div></div>;
+  return <div>
+    <h3>
+      링클웹
+    </h3>
+    <p className="des">made by s.Artemia</p>
+  </div>;
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA2qhcLTk-tfsYCtU3rihcxWqIjRhuKTfo",
+    authDomain: "lingcl-587c6.firebaseapp.com",
+    projectId: "lingcl-587c6",
+    storageBucket: "lingcl-587c6.appspot.com",
+    messagingSenderId: "475689959694",
+    appId: "1:475689959694:web:9f6fa9aaed71a8718bd75f"
+};
+
+export default firebase.initializeApp(firebaseConfig)
